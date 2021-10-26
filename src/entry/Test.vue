7585a0c3f5a0f52e85fb1b751f076540bf7a90c2
Node Sans <node189@icloud.com>
2021-10-26 20:21:27 +0800

    添加了报名页面

--- src/entry/Test.vue
+++ src/entry/Test.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import Info from "../pages/Info.vue";
 import Register from "../pages/Register.vue";
 </script>
 
 <template>
-  <info></info>
-  <register></register>
+    <register></register>
 </template>
 
-<style></style>
+<style>
+</style>
--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
+import { NMessageProvider, NCard } from 'naive-ui'
 import SubmitUserInfo from "/src/components/user/SubmitUserInfo.vue";
 
 </script>
 
 <template>
-  <submit-user-info></submit-user-info>
+    <n-message-provider>
+        <n-card title="报名信息 &nbsp; 💻" style="margin: 4% auto; width: 93%;">
+            <submit-user-info></submit-user-info>
+        </n-card>
+    </n-message-provider>
 </template>
 
-<style></style>
+<style>
+</style>
