191da80675a64ed4cfc93d786663ecda0c31c08d
Node Sans <node189@icloud.com>
2021-11-02 00:05:19 +0800

    报名信息页面样式调整

--- src/components/user/SubmitStudentInfo.vue
+++ src/components/user/SubmitStudentInfo.vue
@@ -81,53 +81,51 @@ function handleValidateClick() {
 </script>
 
 <template>
-    <n-card>
-        <n-form :model="formValue" :rules="rules" ref="formRef" style="margin: 10px auto 0">
-            <n-form-item label="姓名" path="name">
-                <n-input placeholder="请输入姓名" v-model:value="formValue.name" />
-            </n-form-item>
+    <n-form :model="formValue" :rules="rules" ref="formRef" style="margin: 10px auto 0">
+        <n-form-item label="姓名" path="name">
+            <n-input placeholder="请输入姓名" v-model:value="formValue.name" />
+        </n-form-item>
 
-            <n-form-item label="性别" path="gender">
-                <n-radio-group v-model:value="formValue.gender">
-                    <n-radio-button value="1">男</n-radio-button>
-                    <n-radio-button value="0">女</n-radio-button>
-                </n-radio-group>
-            </n-form-item>
+        <n-form-item label="性别" path="gender">
+            <n-radio-group v-model:value="formValue.gender">
+                <n-radio-button value="1">男</n-radio-button>
+                <n-radio-button value="0">女</n-radio-button>
+            </n-radio-group>
+        </n-form-item>
 
-            <n-form-item label="校区" path="campus">
-                <n-radio-group v-model:value="formValue.campus">
-                    <n-radio-button value="1">朝晖</n-radio-button>
-                    <n-radio-button value="2">屏峰</n-radio-button>
-                    <n-radio-button value="3">莫干山</n-radio-button>
-                </n-radio-group>
-            </n-form-item>
+        <n-form-item label="校区" path="campus">
+            <n-radio-group v-model:value="formValue.campus">
+                <n-radio-button value="1">朝晖</n-radio-button>
+                <n-radio-button value="2">屏峰</n-radio-button>
+                <n-radio-button value="3">莫干山</n-radio-button>
+            </n-radio-group>
+        </n-form-item>
 
-            <n-form-item label="学号" path="stu_id">
-                <n-input placeholder="请输入学号" v-model:value="formValue.stu_id" />
-            </n-form-item>
+        <n-form-item label="学号" path="stu_id">
+            <n-input placeholder="请输入学号" v-model:value="formValue.stu_id" />
+        </n-form-item>
 
-            <n-form-item label="身份证号" path="id">
-                <n-input placeholder="请输入身份证号" v-model:value="formValue.id" />
-            </n-form-item>
-            <n-form-item label="电话号码" path="phone">
-                <n-input placeholder="请输入电话号码" v-model:value="formValue.phone" />
-            </n-form-item>
-            <n-form-item label="微信号(可选)" path="wechat">
-                <n-input placeholder="请输入微信号" v-model:value="formValue.wechat" />
-            </n-form-item>
-            <n-form-item label="QQ号(可选)" path="qq">
-                <n-input placeholder="请输入QQ号" v-model:value="formValue.qq" />
-            </n-form-item>
-            <n-form-item>
-                <n-button
-                    @click="handleValidateClick"
-                    attr-type="button"
-                    style="margin: auto; width: 100%"
-                    type="primary"
-                >报名</n-button>
-            </n-form-item>
-        </n-form>
-    </n-card>
+        <n-form-item label="身份证号" path="id">
+            <n-input placeholder="请输入身份证号" v-model:value="formValue.id" />
+        </n-form-item>
+        <n-form-item label="电话号码" path="phone">
+            <n-input placeholder="请输入电话号码" v-model:value="formValue.phone" />
+        </n-form-item>
+        <n-form-item label="微信号(可选)" path="wechat">
+            <n-input placeholder="请输入微信号" v-model:value="formValue.wechat" />
+        </n-form-item>
+        <n-form-item label="QQ号(可选)" path="qq">
+            <n-input placeholder="请输入QQ号" v-model:value="formValue.qq" />
+        </n-form-item>
+        <n-form-item>
+            <n-button
+                @click="handleValidateClick"
+                attr-type="button"
+                style="margin: auto; width: 100%"
+                type="primary"
+            >报名</n-button>
+        </n-form-item>
+    </n-form>
 </template>
 
 <style>
