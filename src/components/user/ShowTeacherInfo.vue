44fd7ba0c5019c6f4aeddbbcfaa72de8d4be9181
Node Sans <node189@icloud.com>
2021-11-02 11:41:23 +0800

    创建教职工个人信息展示

---
+++ src/components/user/ShowTeacherInfo.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { NTable, NButton } from 'naive-ui';
+
+function jumpToSubmitUserInfo() {
+
+}
+</script>
+
+<template>
+    <n-table :bordered="true" style="margin-top: 10px;" :single-line="false">
+        <tbody>
+            <tr>
+                <td class="left-item">
+                    <strong>姓名</strong>
+                </td>
+                <td class="right-item">Node Sans</td>
+            </tr>
+
+            <tr>
+                <td class="left-item">
+                    <strong>学号</strong>
+                </td>
+                <td class="right-item">20200334011</td>
+            </tr>
+
+            <td class="left-item">
+                <strong>电话</strong>
+            </td>
+
+            <td class="right-item">1776956288</td>
+        </tbody>
+    </n-table>
+
+    <n-button
+        style="margin-top: 20px;  width: 100%;"
+        type="primary"
+        @click="jumpToSubmitUserInfo"
+    >修改信息</n-button>
+</template>
+
+<style>
+.left-item {
+    text-align: center;
+    width: 40%;
+}
+.right-item {
+    text-align: center;
+}
+.right-teammate-item {
+    text-align: center;
+}
+</style>
--- src/pages/Info.vue
+++ src/pages/Info.vue
@@ -1,33 +1,24 @@
 <script setup lang="ts">
 import { NCard, NTabs, NTabPane } from 'naive-ui';
+import { userInfo } from 'os';
 import { RouterView, useRouter } from 'vue-router';
-import NotJoin from '/src/components/team/NotJoin.vue'
-import JoinTeam from '/src/components/team/JoinTeam.vue';
-import CreateTeam from '/src/components/team/CreateTeam.vue';
-import TeamInfo from '/src/components/team/TeamInfo.vue';
-import UpdateTeam from '/src/components/team/UpdateTeam.vue';
-import SubmitStudentInfo from "/src/components/user/SubmitStudentInfo.vue";
-import ShowUserInfo from '../components/user/ShowUserInfo.vue';
-import { ref } from 'vue';
 
-// 添加团队信息相关的组件的路由
 const router = useRouter()
-// TODO: 添加个人信息相关路由
-router.addRoute({ path: "/user/showinfo", component: ShowUserInfo })
-// 添加团队信息相关路由
-router.addRoute({ path: "/team/notjoin", component: NotJoin })
-router.addRoute({ path: "/team/join", component: JoinTeam })
-router.addRoute({ path: "/team/create", component: CreateTeam })
-router.addRoute({ path: "/team/teaminfo", component: TeamInfo })
-router.addRoute({ path: "/team/updateteam", component: UpdateTeam })
-router.addRoute({ path: "/team/submit", component: SubmitStudentInfo })
 
-// router.replace("/user/showinfo") // 默认展示个人信息
+// 根据是校友还是学生来选择不同的页面展示
+let userInfoRoute = ""
+if (localStorage.getItem("campus") === "5") {
+    userInfoRoute = "/info/user/showteacher" // 教职工信息展示
+} else {
+    userInfoRoute = "/info/user/showstudent" // 学生信息展示
+}
+
+router.replace(userInfoRoute) // 默认展示个人信息
 function changeTab(value: string) {
-    if (value === "team") 
-        router.replace("/team/teaminfo") // TODO: 设置默认路由为已经加入状态
+    if (value === "team")
+        router.replace("/info/team/") // TODO: 设置默认路由为已经加入状态
     else if (value === "personal") {
-        router.replace("/user/showinfo") 
+        router.replace(userInfoRoute)
     }
 }
 </script>
@@ -36,10 +27,10 @@ function changeTab(value: string) {
     <n-card title="毅行信息管理 🚀" style="margin: 4% auto; width: 93%;">
         <n-tabs @update:value="changeTab" type="line">
             <n-tab-pane name="personal" tab="个人信息">
-                <!-- <router-view></router-view> -->
+                <router-view></router-view>
             </n-tab-pane>
             <n-tab-pane name="team" tab="团队信息">
-                <!-- <router-view></router-view> -->
+                <router-view></router-view>
             </n-tab-pane>
         </n-tabs>
     </n-card>
