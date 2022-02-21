b5426e2265a0200da8eae51f570ae4d4b1a09b8c
Node Sans <node189@icloud.com>
2022-02-21 17:05:20 +0800

    创建小工具文件

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -3,22 +3,10 @@ import { NSpace, NSpin } from 'naive-ui'
 import { useRouter } from 'vue-router'
 import Server from '../config/Server'
 import { onMounted } from '@vue/runtime-core'
+import { getQueryVariable } from '../utility'
 
 // 变量定义
 const router = useRouter()
-
-// 函数定义
-function getQueryVariable(variable: string): string {
-  let query = window.location.search.substring(1)
-  let vars = query.split('&')
-  for (let i = 0; i < vars.length; i++) {
-    let pair = vars[i].split('=')
-    if (pair[0] == variable) {
-      return pair[1]
-    }
-  }
-  return ''
-}
 
 // 挂载函数
 onMounted(() => {
@@ -32,7 +20,7 @@ onMounted(() => {
     if (jwtToken == '') {
       // 如果本地没有 token 并且还没有 token 参数的话
       const oauthUrl = Server.urlPrefix + Server.apiMap['basic']['oauth']
-      window.location.href = oauthUrl
+      window.location.replace(oauthUrl)
     } else {
       // 如果有后端重定向时回传的 token 的话
       localStorage.setItem('jwt', jwtToken)
---
+++ src/utility.ts
@@ -0,0 +1,11 @@
+export function getQueryVariable(variable: string): string {
+  let query = window.location.search.substring(1)
+  let vars = query.split('&')
+  for (let i = 0; i < vars.length; i++) {
+    let pair = vars[i].split('=')
+    if (pair[0] == variable) {
+      return pair[1]
+    }
+  }
+  return ''
+}
